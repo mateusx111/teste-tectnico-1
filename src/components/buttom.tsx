59155ca7667c474f50c4b0bcd86ce5114c595409
MateusSouza <msouza954@gmail.com>
2024-02-29 15:56:04 -0300

    web

---
+++ src/components/buttom.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const buttom = () => {
+  return (
+    <button className="bg-green-100 text-xs font-medium text-green-800 py-2 px-4 rounded-md">
+      Available
+    </button>
+  );
+};
+
+export default buttom;
